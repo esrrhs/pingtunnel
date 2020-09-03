aa88696c37dc9b7e61a48e16dad352f359c4a83c
esrrhs <esrrhs@163.com>
2020-09-03 09:36:44 +0800

    add

--- pack.sh
+++ pack.sh
@@ -31,8 +31,3 @@ zip pingtunnel_mips64.zip pingtunnel
 GOOS=linux GOARCH=mips64le go build
 zip pingtunnel_mips64le.zip pingtunnel
 
-GOOS=linux GOARCH=mips64p32 go build
-zip pingtunnel_mips64p32.zip pingtunnel
-
-GOOS=linux GOARCH=mips64p32le go build
-zip pingtunnel_mips64p32le.zip pingtunnel
